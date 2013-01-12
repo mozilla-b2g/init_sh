88b1cf5e28b1eb4ee29c46a6e7a3ca8af8e377e8
Dees_Troy <dees_troy@teamw.in>
2013-01-12 20:14:08 +0000

    extract_elf_ramdisk: Initial commit

--- Android.mk
+++ Android.mk
@@ -44,11 +44,18 @@ LOCAL_SRC_FILES    := busybox
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := extract_elf_ramdisk
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/sbin
-LOCAL_MODULE_CLASS := EXECUTABLES
-LOCAL_SRC_FILES    := extract_elf_ramdisk
-include $(BUILD_PREBUILT)
+LOCAL_SRC_FILES:= \
+	extract_elf_ramdisk.c \
+	system.c
+LOCAL_FORCE_STATIC_EXECUTABLE := true
+LOCAL_STATIC_LIBRARIES = libelf libc libm
+LOCAL_C_INCLUDES := external/elfutils/libelf
+LOCAL_CFLAGS:= -g -c -W
+LOCAL_MODULE:=extract_elf_ramdisk
+LOCAL_MODULE_TAGS:= eng
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
+include $(BUILD_EXECUTABLE)
 
 # Look at http://stackoverflow.com/questions/10654049/how-does-module-dependency-work-in-android
 # ALL_DEFAULT_INSTALLED_MODULES += $(SU_BINARY)-post
