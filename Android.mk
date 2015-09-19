49bd8443cf694d3a0a0c106db7a127f39e221b82
Adam Farden <adam@farden.cz>
2015-09-19 11:03:03 +0200

    Bug 1203612 - Add extract_elf_ramdisk to build r=dhylands

--- Android.mk
+++ Android.mk
@@ -44,18 +44,17 @@ LOCAL_SRC_FILES    := busybox
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-
-LOCAL_SRC_FILES := extract_elf_ramdisk.c
+LOCAL_MODULE       := extract_elf_ramdisk
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)/sbin
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS  := optional
+LOCAL_SRC_FILES    := extract_elf_ramdisk.c
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libelf libc libm libz
 LOCAL_C_INCLUDES := \
 	external/elfutils/libelf \
 	external/zlib
 LOCAL_CFLAGS := -g -c -W
-LOCAL_MODULE := extract_elf_ramdisk
-LOCAL_MODULE_TAGS := eng
-LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
-LOCAL_MODULE_PATH := $(PRODUCT_OUT)/utilities
 include $(BUILD_EXECUTABLE)
 
 # Look at http://stackoverflow.com/questions/10654049/how-does-module-dependency-work-in-android
